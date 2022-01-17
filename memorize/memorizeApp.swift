0d434d27523765ef2087eafcf0dbf279f8bf0397
allan galdino <galdinoallancodes@gmail.com>
2022-01-17 19:05:20 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by allan galdino on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
